90b8b93c5d9c9059296437530d3ff818c53d1df4
oleksii movchaniuk <oleksii.movchaniuk@fractal.ai>
2023-06-03 09:50:56 +0100

    new dataset added

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -111,27 +111,71 @@ function btnSettings(event) {
 
 async function insertData() {
   try {
-    await Excel.run(async (context) => {
+      await Excel.run(async (context) => {
       let sheet = context.workbook.worksheets.getActiveWorksheet();
-      let expensesTable = sheet.tables.add("A1:D1", true /*hasHeaders*/);
+      // Adjusted the range to have enough columns for each quarter from Q1_2018 to Q4_2024
+      let expensesTable = sheet.tables.add("A1:AG1", true /*hasHeaders*/);
       expensesTable.name = "ExpensesTable";
 
-      expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
-
+      expensesTable.getHeaderRowRange().values = [
+        [
+          "Geography",
+          "Product",
+          "Brand",
+          "Size",
+          "Fact",
+          "Q1_2018",
+          "Q2_2018",
+          "Q3_2018",
+          "Q4_2018",
+          "Q1_2019",
+          "Q2_2019",
+          "Q3_2019",
+          "Q4_2019",
+          "Q1_2020",
+          "Q2_2020",
+          "Q3_2020",
+          "Q4_2020",
+          "Q1_2021",
+          "Q2_2021",
+          "Q3_2021",
+          "Q4_2021",
+          "Q1_2022",
+          "Q2_2022",
+          "Q3_2022",
+          "Q4_2022",
+          "Q1_2023",
+          "Q2_2023",
+          "Q3_2023",
+          "Q4_2023",
+          "Q1_2024",
+          "Q2_2024",
+          "Q3_2024",
+          "Q4_2024"
+        ],
+      ];
+  
       expensesTable.rows.add(null /*add rows to the end of the table*/, [
-        ["1/1/2017", "The Phone Company", "Communications", "$120"],
-        ["1/2/2017", "Northwind Electric Cars", "Transportation", "$142"],
-        ["1/5/2017", "Best For You Organics Company", "Groceries", "$27"],
-        ["1/10/2017", "Coho Vineyard", "Restaurant", "$33"],
-        ["1/11/2017", "Bellows College", "Education", "$350"],
-        ["1/15/2017", "Trey Research", "Other", "$135"],
-        ["1/15/2017", "Best For You Organics Company", "Groceries", "$97"],
+        ["▼ Total", "", "", "", "", 600, 640, 650, 675, 710, 740, 770, 800, 830, 860, 890, 920, 950, 980, 1010, 1040, 1070, 1100, 1130, 1160, 1190, 1220, 1250, 1280, 1310, 1340, 1370, 1400],
+        ["▶ North America", "Product A", "Brand X", "Size 1", "Fact 1", 100, 120, 110, 115, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360],
+        ["▶ Europe", "Product B", "Brand Y", "Size 2", "Fact 2", 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470],
+        ["▶ Asia", "Product C", "Brand Z", "Size 3", "Fact 3", 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570],
       ]);
+  
+      // Add the Table Style
+      expensesTable.style = "TableStyleLight9";
 
-      /*if (Office.context.requirements.isSetSupported("ExcelApi", "1.2")) {
+      // Get the range of cells you want to format.
+      let range = sheet.getRange("R2:Y5");
+
+      // Set the fill color.
+      range.format.fill.color = "#FFF2CC";
+
+      if (Office.context.requirements.isSetSupported("ExcelApi", "1.2")) {
         sheet.getUsedRange().format.autofitColumns();
         sheet.getUsedRange().format.autofitRows();
-      }*/
+      }
+
       context.sync().then(() => {
         monitorSheetChanges();
         return context.sync();
